835cd6cc612e974ecfd3c40742687e8a7ae8addf
your name <abukarakiomar@gmail.com>
2024-06-03 23:11:30 +0300

    change the map and add a local storage in contact page

--- js/contact-script.js
+++ js/contact-script.js
@@ -1,43 +1,72 @@
-let mapOptions={
-    center:[31.97543, 35.85977],
-    zoom:10
-}
-
-// creating a map 
-
-let map=new L.map('map',mapOptions);
-let layer =new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-map.addLayer(layer);
 
 
-let name =document.querySelector(".form .form-box #name");
-let email =document.querySelector(".form .form-box #email");
-let subject =document.querySelector(".form .form-box #subject");
-let message =document.querySelector(".form .form-box #message");
+let name = document.querySelector(".form .form-box #name");
+        let email = document.querySelector(".form .form-box #email");
+        let subject = document.querySelector(".form .form-box #subject");
+        let message = document.querySelector(".form .form-box #message");
 
+        function loadFormData() {
+            if (localStorage.getItem('name')) {
+                name.value = localStorage.getItem('name');
+            }
+            if (localStorage.getItem('email')) {
+                email.value = localStorage.getItem('email');
+            }
+            if (localStorage.getItem('subject')) {
+                subject.value = localStorage.getItem('subject');
+            }
+            if (localStorage.getItem('message')) {
+                message.value = localStorage.getItem('message');
+            }
+        }
 
-function submitBtn(){
+        function addToQueue() {
+            let visitorCount = localStorage.getItem('visitorCount');
 
-   if(name.value && email.value && subject.value && message.value !==""){
-    Swal.fire({
-        position: "top-center",
-        icon: "success",
-        title: `Thanks ${name.value} , we will contact you soon at` ,
-        showConfirmButton: false,
-        timer: 15000
-    });
-    name.value ="";
-    email.value="";
-    subject.value="";
-    message.value="";
-   }else
-   {
-    Swal.fire({
-        position: "top-center",
-        icon: "error",
-        title: `please fill the empty feilds` ,
-        showConfirmButton: false,
-        timer: 15000
-    });
-   }
-    }
+            if (visitorCount === null) {
+                visitorCount = 1;
+            } else {
+                visitorCount = parseInt(visitorCount, 10) + 1;
+            }
+
+            localStorage.setItem('visitorCount', visitorCount);
+            document.getElementById('visitor-count').innerText = visitorCount;
+        }
+
+        function submitBtn() {
+            console.log(name.value);
+            console.log(email.value);
+
+            if (name.value && email.value && subject.value && message.value !== "") {
+                Swal.fire({
+                    position: "top-center",
+                    icon: "success",
+                    title: `Thanks ${name.value} , your position in queue : ${localStorage.getItem('visitorCount')}`,
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+
+                localStorage.setItem('name', name.value);
+                localStorage.setItem('email', email.value);
+                localStorage.setItem('subject', subject.value);
+                localStorage.setItem('message', message.value);
+
+                name.value = "";
+                email.value = "";
+                subject.value = "";
+                message.value = "";
+            } else {
+                Swal.fire({
+                    position: "top-center",
+                    icon: "error",
+                    title: `Please fill the empty fields`,
+                    showConfirmButton: false,
+                    timer: 2500
+                });
+            }
+        }
+
+        window.onload = function() {
+            loadFormData();
+            addToQueue();
+        };
